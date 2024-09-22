f026ad14b4be5b0353f53566a2379d1b0285b353
batufidan <batuhan.fidan@ogr.iu.edu.tr>
2024-09-22 15:59:28 +0300

    [ADD] project setup

---
+++ src/sections/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <a>
+          <img src="/images/xora.svg" width={150} height={150} alt="logo" />
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
